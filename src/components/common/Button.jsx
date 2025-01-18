f3536ae63ddb5cadccd564de4fe9422c0f4db935
Yaminikanta-code <yaminikantaprof1@gmail.com>
2025-01-19 00:00:33 +0530

    Built Auth UI

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const Button = ({
   children,
@@ -7,6 +6,7 @@ const Button = ({
   className,
   type = "button",
   disabled = false,
+  ...props
 }) => {
   return (
     <button
@@ -14,6 +14,7 @@ const Button = ({
       onClick={onClick}
       disabled={disabled}
       className={`px-4 py-2 rounded-md bg-primary text-primary-foreground shadow hover:bg-primary/90 disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
+      {...props}
     >
       {children}
     </button>
