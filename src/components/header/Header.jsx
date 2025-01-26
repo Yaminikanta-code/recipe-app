eea24dddaf6f692746b651fa8ad8158fd6617dcc
Yaminikanta-code <yaminikantaprof1@gmail.com>
2025-01-26 22:27:25 +0530

    redux store and auth slice  set-up

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { MenuBar, Avatar } from "../common";
 import Logo from "./Logo";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { user } = useSelector((state) => state.auth);
   return (
     <MenuBar className="justify-between">
       <Logo />
